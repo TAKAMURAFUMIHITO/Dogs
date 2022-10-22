9c2a5df99c5ee2b078a063436eaae1deef345e5d
EC2 Default User <fumipro0913@gmail.com>
2022-10-22 18:35:34 +0000

    [Add]過去一週間以内のいいね数順でランキング

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -20,7 +20,13 @@ class Public::PostsController < ApplicationController
   def index
     @today = Date.today #今日の日付を取得
     @now = Time.now     #現在時刻を取得
-    @posts = Post.all.page(params[:page]).per(15)
+    to  = Time.current.at_end_of_day
+    from  = (to - 6.day).at_beginning_of_day
+    posts = Post.all.sort {|a,b|                      #過去１週間のいいね数順に表示される
+      b.likes.where(created_at: from...to).size <=>
+      a.likes.where(created_at: from...to).size
+    }
+    @posts = Kaminari.paginate_array(posts).page(params[:page]).per(15)  #ページネーション
   end
 
   def show
